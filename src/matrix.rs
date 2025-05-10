74429b64778734d0b5c5fe8927fabde9e85b3efd
starry_tree <starry_tree@outlook.com>
2025-05-10 17:54:43 +0800

    matrix multiply

---
+++ src/matrix.rs
@@ -0,0 +1,110 @@
+use anyhow::Result;
+use anyhow::anyhow;
+use std::fmt;
+use std::ops::Add;
+use std::ops::AddAssign;
+use std::ops::Mul;
+
+// [[1,2],[1,2],[1,2]]=>[1,2,1,2,1,2]
+
+pub struct Matrix<T> {
+    data: Vec<T>,
+    row: usize,
+    col: usize,
+}
+
+pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
+where
+    T: Add<Output = T> + AddAssign + Mul<Output = T> + Copy + Default,
+{
+    if a.col != b.row {
+        return Err(anyhow!("matrix multiply error"));
+    }
+
+    let mut data = vec![T::default(); a.row * b.col];
+    for i in 0..a.row {
+        for j in 0..b.col {
+            for k in 0..a.col {
+                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            }
+        }
+    }
+
+    Ok(Matrix {
+        data,
+        row: a.row,
+        col: b.col,
+    })
+}
+
+impl<T: fmt::Debug> Matrix<T> {
+    pub fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
+        Self {
+            data: data.into(),
+            row,
+            col,
+        }
+    }
+}
+
+impl<T> fmt::Display for Matrix<T>
+where
+    T: fmt::Display,
+{
+    // display as {1 2 3, 4 5 6, 7 8 9}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{{")?;
+        for i in 0..self.row {
+            for j in 0..self.col {
+                write!(f, "{:}", self.data[i * self.col + j])?;
+                if j != self.col - 1 {
+                    write!(f, " ")?;
+                }
+            }
+            if i != self.row - 1 {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "}}")?;
+
+        Ok(())
+    }
+}
+
+impl<T> fmt::Debug for Matrix<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Matrix(row={}, {}, {})", self.row, self.col, self)
+    }
+}
+
+#[allow(dead_code)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_matrix_multiply() -> Result<()> {
+        let a = Matrix::new(&[1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new(&[1, 2, 3, 4, 5, 6], 3, 2);
+        let c = multiply(&a, &b).unwrap();
+        assert_eq!(c.row, 2);
+        assert_eq!(c.col, 2);
+        assert_eq!(c.data, &[22, 28, 49, 64]);
+        assert_eq!(format!("{:?}", c), "Matrix(row=2, 2, {22 28, 49 64})");
+        Ok(())
+    }
+
+    #[test]
+    fn test_matrix_display() -> Result<()> {
+        let a = Matrix::new(&[1, 2, 3, 4],  2, 2);
+        let b = Matrix::new(&[1, 2, 3, 4], 2, 2);
+        let c = multiply(&a, &b)?;
+        assert_eq!(c.row, 2);
+        assert_eq!(c.col, 2);
+        assert_eq!(c.data, &[7, 10, 15, 22]);
+        assert_eq!(format!("{:}", c), "{7 10, 15 22}");
+        Ok(())
+    }
+}
