16b1af33b3e290b3c602e8d3318a825ad64a6c6c
starry_tree <starry_tree@outlook.com>
2025-05-10 13:11:11 +0800

    thread join end

--- examples/thread1.rs
+++ examples/thread1.rs
@@ -20,17 +20,22 @@ fn main() -> Result<()> {
         let tx = tx.clone();
         thread::spawn(move || producer(i, tx));
     }
+    drop(tx);
 
     // 创建consumer
     let consumer = thread::spawn(move || {
         for msg in rx {
             println!("consumer: {:?}", msg);
         }
+        println!("consumer exit");
+        42
     });
 
-    consumer
+    let secret = consumer
         .join()
         .map_err(|e| anyhow!("consumer join error: {:?}", e))?;
+
+    println!("secret: {}", secret);
 
     Ok(())
 }
@@ -41,7 +46,13 @@ fn producer(idx: usize, tx: mpsc::Sender<Msg>) -> Result<()> {
         tx.send(Msg::new(idx, value))?;
         let sleep_time = rand::random::<u8>() as u64 * 10;
         thread::sleep(Duration::from_millis(sleep_time));
+        if rand::random::<u8>() % 10 == 0 {
+            println!("producer {} exit", idx);
+            break;
+        }
     }
+
+    Ok(())
 }
 
 impl Msg {
