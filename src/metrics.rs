c0ae04ca6af5c43bafcb0a807796848409e2632e
starry_tree <starry_tree@outlook.com>
2025-05-17 18:29:24 +0800

    hashmap => dashmap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,29 +1,25 @@
 // metrics data structure
 // 基本功能：inc/dec/snapshot
 use anyhow::Result;
-use anyhow::anyhow;
 
+use dashmap::DashMap;
 use std::fmt;
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
+use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&mut self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter += 1;
 
         Ok(())
@@ -35,20 +31,19 @@ impl Metrics {
     //     *counter -= 1;
     // }
 
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
-    }
+    // pub fn snapshot(&self) -> Result<DashMap<String, i64>> {
+    //     Ok(self
+    //         .data
+    //         .read()
+    //         .map_err(|e| anyhow!(e.to_string()))?
+    //         .clone())
+    // }
 }
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let data = self.data.read().map_err(|_e| fmt::Error {})?;
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
         Ok(())
     }
